6b2bde62e13dd6ab0a2367635bd4cbaffb50371e
Trinadh Rongala <sai3nadh@gmail.com>
2025-02-17 21:54:49 +0530

    prod url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://103f-106-66-44-9.ngrok-free.app/api' // Local URL for development
+    // apiUrl: 'https://103f-106-66-44-9.ngrok-free.app/api' // Local URL for development
 
+    apiUrl: '/api'  // Firebase Hosting will forward this to the GCP VM API
     // apiUrl: 'https://your-gcp-url/api' // Your GCP URL for production
   };
   
