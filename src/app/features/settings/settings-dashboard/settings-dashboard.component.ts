3b4a72fb00fe312d0057598cfea3113a16c10954
Trinadh Rongala <sai3nadh@gmail.com>
2025-05-22 23:31:44 +0100

    settings dashboard added all pages

--- src/app/features/settings/settings-dashboard/settings-dashboard.component.ts
+++ src/app/features/settings/settings-dashboard/settings-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings-dashboard',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './settings-dashboard.component.scss'
 })
 export class SettingsDashboardComponent {
+    constructor(private router: Router) {}
 
+ goTo(path: string) {
+    if (path.startsWith('/')) {
+      this.router.navigate([path]);
+    } else {
+      this.router.navigate([`/settings/${path}`]);
+    }
+    // this.router.navigate([`/settings/${path}`]);
+  }
 }
