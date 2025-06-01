3a8f1404f221452af27648e4f83c32686cef6f3e
Trinadh Rongala <sai3nadh@gmail.com>
2025-06-02 00:08:16 +0100

    PrinterBridgeService - isAndroid

--- src/app/features/settings/printer-settings/services/printer.service.ts
+++ src/app/features/settings/printer-settings/services/printer.service.ts
@@ -20,3 +20,19 @@ export class PrinterService {
 //     return this.http.put(this.baseUrl, config);
 //   }
 }
+
+export class PrinterBridgeService {
+    // PrinterBridgeService.printOrder(response);
+    
+  static isAndroid(): boolean {
+    return typeof (window as any).AndroidPrint !== 'undefined';
+  }
+
+  static printOrder(order: any): void {
+    if (this.isAndroid() && (window as any).AndroidPrint.printOrder) {
+      (window as any).AndroidPrint.printOrder(JSON.stringify(order));
+    } else {
+      console.warn('Not running inside Android WebView. Printing skipped.');
+    }
+  }
+}
